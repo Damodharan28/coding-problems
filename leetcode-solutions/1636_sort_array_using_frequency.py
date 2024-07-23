e9df1b651e118da7c6bbeafa20c094e31c4ea791
Damodharan28 <142664753+Damodharan28@users.noreply.github.com>
2024-07-23 20:47:38 +0530

    1636 leetcode solution

--- leetcode-solutions/1636_sort_array_using_frequency.py
+++ leetcode-solutions/1636_sort_array_using_frequency.py
@@ -1,11 +1,16 @@
 class Solution:
     def frequencySort(self, nums):
-        freq=[0]*201
+        # Initialize frequency array
+        freq = [0] * 201  # We use 201 to cover the range from -100 to 100
+
+        # Count frequencies of each number
         for x in nums:
-            freq[x+100]-=1 
-        return sorted(nums, key=lambda x:(freq[x+100], x), reverse=True)
+            freq[x + 100] -= 1  # Increment the frequency count
 
+        # Sort the numbers based on frequency and value
+        sorted_nums = sorted(nums, key=lambda x: (freq[x + 100], x), reverse=True)
+        return sorted_nums
 
-sol = Solution()
+solution = Solution()
 nums = [1,1,2,2,2,3]
-sol.frequencySort(nums)
+solution.frequencySort(nums)
