643a0cee6d24ca3f672b93292f19f7654e4ec732
DamodharanM <142664753+Damodharan28@users.noreply.github.com>
2024-07-24 18:44:20 +0530

    1 two sum

---
+++ leetcode-solutions/1_two_sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        # Get the number of elements in nums
+        size = len(nums)
+        
+        # Loop through each element in nums except the last one
+        for i in range(size - 1):
+            # For each element nums[i], loop through the elements that come after it
+            for j in range(i + 1, size):
+                # Check if the sum of nums[i] and nums[j] equals the target
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        # If no such pair is found, return an empty list
+        return []
+
+sol = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+sol.twoSum(nums, target)
