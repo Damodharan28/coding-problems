7e51936a7a375ca3f128922de3e22612f5858897
DamodharanM <142664753+Damodharan28@users.noreply.github.com>
2024-07-28 19:54:48 +0530

    1334 leetcode solution

--- leetcode-solutions/1334_ smallest_number_of_neighbors_at _threshold_distance.py
+++ leetcode-solutions/1334_ smallest_number_of_neighbors_at _threshold_distance.py
@@ -36,9 +36,3 @@ class Solution:
                 bestCity = i
         
         return bestCity
-
-solution = Solution()
-n = 5
-edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]]
-distanceThreshold = 2
-solution.findTheCity(n, edges, distanceThreshold)
